37d3687e979c788a398ef496a58e44cfcdec5046
jeongwoo903 <www8565@naver.com>
2023-11-29 03:25:29 +0900

    Feat: finish `DefaultPage` layout

--- src/views/admin/default/components/CheckTable.tsx
+++ src/views/admin/default/components/CheckTable.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import CardMenu from "components/card/CardMenu";
+import { MdChevronRight, MdChevronLeft } from "react-icons/md";
 import Checkbox from "components/checkbox";
 import Card from "components/card";
+import { FiSearch } from "react-icons/fi";
 
 import {
   createColumnHelper,
@@ -27,7 +28,7 @@ function CheckTable(props: { tableData: any }) {
     columnHelper.accessor("name", {
       id: "name",
       header: () => (
-        <p className="text-sm font-bold text-gray-600 dark:text-white">NAME</p>
+        <p className="text-sm font-bold text-gray-600 dark:text-white">항목</p>
       ),
       cell: (info: any) => (
         <div className="flex items-center">
@@ -45,8 +46,54 @@ function CheckTable(props: { tableData: any }) {
     columnHelper.accessor("progress", {
       id: "progress",
       header: () => (
+        <p className="text-sm font-bold text-gray-600 dark:text-white">이름</p>
+      ),
+      cell: (info) => (
+        <p className="text-sm font-bold text-navy-700 dark:text-white">
+          {info.getValue()}
+        </p>
+      ),
+    }),
+    columnHelper.accessor("quantity", {
+      id: "quantity",
+      header: () => (
         <p className="text-sm font-bold text-gray-600 dark:text-white">
-          PROGRESS
+          주민등록번호
+        </p>
+      ),
+      cell: (info) => (
+        <p className="text-sm font-bold text-navy-700 dark:text-white">
+          {info.getValue()}
+        </p>
+      ),
+    }),
+    columnHelper.accessor("date", {
+      id: "date",
+      header: () => (
+        <p className="text-sm font-bold text-gray-600 dark:text-white">성별</p>
+      ),
+      cell: (info) => (
+        <p className="text-sm font-bold text-navy-700 dark:text-white">
+          {info.getValue()}
+        </p>
+      ),
+    }),
+    columnHelper.accessor("quantity", {
+      id: "quantity",
+      header: () => (
+        <p className="text-sm font-bold text-gray-600 dark:text-white">주소</p>
+      ),
+      cell: (info) => (
+        <p className="text-sm font-bold text-navy-700 dark:text-white">
+          {info.getValue()}
+        </p>
+      ),
+    }),
+    columnHelper.accessor("quantity", {
+      id: "quantity",
+      header: () => (
+        <p className="text-sm font-bold text-gray-600 dark:text-white">
+          담당의
         </p>
       ),
       cell: (info) => (
@@ -59,7 +106,7 @@ function CheckTable(props: { tableData: any }) {
       id: "quantity",
       header: () => (
         <p className="text-sm font-bold text-gray-600 dark:text-white">
-          QUANTITY
+          마지막 진료일
         </p>
       ),
       cell: (info) => (
@@ -68,10 +115,25 @@ function CheckTable(props: { tableData: any }) {
         </p>
       ),
     }),
-    columnHelper.accessor("date", {
-      id: "date",
+    columnHelper.accessor("quantity", {
+      id: "quantity",
       header: () => (
-        <p className="text-sm font-bold text-gray-600 dark:text-white">DATE</p>
+        <p className="text-sm font-bold text-gray-600 dark:text-white">
+          예약일시
+        </p>
+      ),
+      cell: (info) => (
+        <p className="text-sm font-bold text-navy-700 dark:text-white">
+          {info.getValue()}
+        </p>
+      ),
+    }),
+    columnHelper.accessor("quantity", {
+      id: "quantity",
+      header: () => (
+        <p className="text-sm font-bold text-gray-600 dark:text-white">
+          특이사항
+        </p>
       ),
       cell: (info) => (
         <p className="text-sm font-bold text-navy-700 dark:text-white">
@@ -93,17 +155,41 @@ function CheckTable(props: { tableData: any }) {
     debugTable: true,
   });
   return (
-    <Card extra={"w-full h-full sm:overflow-auto px-6"}>
-      <header className="relative flex items-center justify-between pt-4">
-        <div className="text-xl font-bold text-navy-700 dark:text-white">
-          Check Table
+    <Card extra={"w-full h-full sm:overflow-auto px-6 pb-6"}>
+      <header className="relative flex h-[53px] items-center justify-between pt-[1.9rem]">
+        <div className="title-container">
+          <div className="text-xl font-bold text-navy-700 dark:text-white">
+            전체 환자 목록
+          </div>
+          <p className="text-sm font-bold text-gray-600 dark:text-white">
+            {">"} page 1
+          </p>
         </div>
-
-        <CardMenu />
+        {/* search bar */}
+        <div className="tool-container flex items-center gap-6">
+          <div className="flex h-[42px] items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
+            <p className="pl-3 pr-2 text-xl">
+              <FiSearch className="h-4 w-4 text-gray-400 dark:text-white" />
+            </p>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="block h-full w-full rounded-full bg-lightPrimary text-sm font-medium text-navy-700 outline-none placeholder:!text-gray-400 dark:bg-navy-900 dark:text-white dark:placeholder:!text-white sm:w-fit"
+            />
+          </div>
+          <div className="buttom-container flex justify-end gap-2">
+            <button className="flex flex-row items-center rounded-xl bg-brand-400 px-3 py-3 text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
+              <MdChevronLeft className="text-lg" />
+            </button>
+            <button className="flex flex-row items-center rounded-xl bg-brand-400 px-3 py-3 text-base font-medium text-white transition duration-200 hover:bg-brand-600 active:bg-brand-700 dark:bg-brand-400 dark:text-white dark:hover:bg-brand-300 dark:active:bg-brand-200">
+              <MdChevronRight className="text-lg" />
+            </button>
+          </div>
+        </div>
       </header>
 
-      <div className="mt-8 overflow-x-scroll xl:overflow-x-hidden">
-        <table className="w-full">
+      <div className="mb-4 mt-8 overflow-x-scroll">
+        <table className="w-full overflow-x-auto">
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id} className="!border-px !border-gray-400">
@@ -113,7 +199,7 @@ function CheckTable(props: { tableData: any }) {
                       key={header.id}
                       colSpan={header.colSpan}
                       onClick={header.column.getToggleSortingHandler()}
-                      className="cursor-pointer border-b-[1px] border-gray-200 pt-4 pb-2 pr-4 text-start"
+                      className="cursor-pointer border-b-[1px] border-gray-200 pb-2 pr-4 pt-4 text-start"
                     >
                       <div className="items-center justify-between text-xs text-gray-200">
                         {flexRender(
@@ -134,7 +220,7 @@ function CheckTable(props: { tableData: any }) {
           <tbody>
             {table
               .getRowModel()
-              .rows.slice(0, 5)
+              .rows.slice(0, 10) // n개 만큼 자름
               .map((row) => {
                 return (
                   <tr key={row.id}>
