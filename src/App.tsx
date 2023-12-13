05069e989dd4da22fe044915f23b3abcaad9387b
whaleflyingsky <sunbeanp@hanyang.ac.kr>
2023-12-13 22:54:58 +0900

    Chore: add package manager react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import AdminLayout from "pages/admin";
 import StartPage from "pages/start";
 import AuthLayout from "pages/auth";
+import { Toaster } from "react-hot-toast";
+import SignIn from "pages/sign-in/Index";
+import SignUp from "pages/sign-up/Index";
+import Waiting from "pages/waiting/Index";
 const App = () => {
   return (
-    <Routes>
-      <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="admin/*" element={<AdminLayout />} />
-      <Route path="/" element={<StartPage />} />
-    </Routes>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route path="auth/*" element={<AuthLayout />} />
+        <Route path="admin/*" element={<AdminLayout />} />
+        <Route path="/" element={<StartPage />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/waiting" element={<Waiting />} />
+      </Routes>
+    </>
   );
 };
 
